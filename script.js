58458ea3ce72f16335c83fa46a2f4d812c921f35
Alex Bilba <alex.shanless@gmail.com>
2021-04-05 18:19:49 -0700

    done

--- script.js
+++ script.js
@@ -15,10 +15,13 @@ for (let i = 0; i < SQUARES; i++) {
 function setColor(element) {
   const color = getRandomColor();
   element.style.background = color;
-  element.style.boxShadow = `0 0 2px ${color} 0 0 10px`;
+  element.style.boxShadow = `0 0 2px ${color} 0 0 10px ${color}`;
 }
 
-function removeColor(element) {}
+function removeColor(element) {
+    element.style.background = '#1d1d1d';
+    element.style.boxShadow = '0 0 2px #000'
+}
 
 function getRandomColor() {
   return colors[Math.floor(Math.random() * colors.length)];
